dcf54c5483a4b8afd98f4b77269cbe924d8136cb
Katherine Wren <kwren@outlook.com>
2019-05-02 14:31:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
+  end
+  
+  def string_split
+    self.split(//)
   end
 
   def count_sentences
-
+    
+    split_sentence.count
   end
 end
